6e869982b02c04be8d108091ca0b2e58e8d57d6c
ted <6bangs@gmail.com>
2019-04-06 17:27:43 -0400

    Tasks 28 and 29.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -10,10 +10,36 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
 
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval) = addTimeIntervals(timeIntervals.timeInterval, timeIntervals.count)
+
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
 
-class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>
+data class RepeatedTimeInterval(val timeInterval: TimeInterval, val count: Int)
+
+operator fun TimeInterval.times(count: Int) = RepeatedTimeInterval(this, count)
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate)
+    : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            private var current: MyDate = start
+
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                val next = current
+                current = current.nextDay()
+
+                return next
+            }
+        }
+    }
+}
