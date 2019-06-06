c3baf852017271161441d7114a6d053397763e37
ted <6bangs@gmail.com>
2019-06-06 17:06:18 -0400

    Tasks 35 - 37.

--- src/iv_properties/n35HowDelegatesWork.kt
+++ src/iv_properties/n35HowDelegatesWork.kt
@@ -29,8 +29,11 @@ class D {
 class EffectiveDate<R> : ReadWriteProperty<R, MyDate> {
     var timeInMillis: Long? = null
 
-    operator override fun getValue(thisRef: R, property: KProperty<*>): MyDate = todoTask35()
-    operator override fun setValue(thisRef: R, property: KProperty<*>, value: MyDate) = todoTask35()
+    override fun getValue(thisRef: R, property: KProperty<*>): MyDate = (timeInMillis ?: 0).toDate()
+
+    override fun setValue(thisRef: R, property: KProperty<*>, value: MyDate) {
+        timeInMillis = value.toMillis()
+    }
 }
 
 fun MyDate.toMillis(): Long {
